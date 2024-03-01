8a075dc3c44815da5ba1b2e798b9254750a6c19f
Eden <s30ngb1n@gmail.com>
2024-03-01 19:14:48 +0900

    feat: pokemon api get

---
+++ src/pages/home.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PokemonCard from "../components/pokemonCard";
+
+const Home = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const allPokemonData: any = [];
+      for (let i = 1; i <= 151; i++) {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${i}`
+        );
+        const speciesResponse = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon-species/${i}`
+        );
+        const koreanName = speciesResponse.data.names.find(
+          (name: any) => name.language.name === "ko"
+        );
+        allPokemonData.push({ ...response.data, korean_name: koreanName.name });
+      }
+      setPokemonData(allPokemonData);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {pokemonData.map((pokemon: any) => (
+        <PokemonCard
+          key={pokemon.id}
+          name={pokemon.korean_name}
+          id={pokemon.id}
+          image={pokemon.sprites.front_default}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
