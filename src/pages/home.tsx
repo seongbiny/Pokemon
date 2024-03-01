e5ccdb78a4327079a3787827dff2e3566db27f97
Eden <s30ngb1n@gmail.com>
2024-03-01 21:20:06 +0900

    feat: 무한스크롤 기능

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,42 +1,89 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import PokemonCard from "../components/pokemonCard";
 import "./home.css";
 
 const Home = () => {
-  const [pokemonData, setPokemonData] = useState([]);
+  const pokemonPerPage = 5;
+
+  const [pokemonData, setPokemonData] = useState<any>([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const observer = useRef<any>();
 
   useEffect(() => {
-    const fetchData = async () => {
+    fetchData();
+  }, [page]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
       const allPokemonData: any = [];
-      for (let i = 1; i <= 151; i++) {
+      for (let i = 1; i <= pokemonPerPage; i++) {
         const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${i}`
+          `https://pokeapi.co/api/v2/pokemon/${i + pokemonPerPage * page}`
         );
         const speciesResponse = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon-species/${i}`
+          `https://pokeapi.co/api/v2/pokemon-species/${
+            i + pokemonPerPage * page
+          }`
         );
         const koreanName = speciesResponse.data.names.find(
           (name: any) => name.language.name === "ko"
         );
-        allPokemonData.push({ ...response.data, korean_name: koreanName.name });
+        allPokemonData.push({
+          ...response.data,
+          korean_name: koreanName.name,
+        });
       }
-      setPokemonData(allPokemonData);
+      setPokemonData((prevData: any) => [...prevData, ...allPokemonData]);
+    } catch (error) {
+      console.error("Error fetching Pokemon data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+    if (entries[0].isIntersecting && !loading) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
     };
 
-    fetchData();
-  }, []);
+    observer.current = new IntersectionObserver(handleIntersection, options);
+
+    if (observer.current) {
+      observer.current.observe(document.getElementById("observer-element"));
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, [pokemonData]);
 
   return (
     <div className="container">
-      {pokemonData.map((pokemon: any) => (
-        <PokemonCard
-          key={pokemon.id}
-          name={pokemon.korean_name}
-          id={pokemon.id}
-          image={pokemon.sprites.front_default}
-        />
-      ))}
+      {pokemonData &&
+        pokemonData.map((pokemon: any, idx: any) => (
+          <PokemonCard
+            key={idx}
+            name={pokemon.korean_name}
+            id={pokemon.id}
+            image={pokemon.sprites.front_default}
+          />
+        ))}
+      <div id="observer-element" style={{ height: "10px" }}></div>
+      {loading && <p>Loading...</p>}
     </div>
   );
 };
